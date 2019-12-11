f206289f6b40840a6a10ebcfbd72c5539e1a9721
Osarumen Alohan <cyrilalohan@gmail.com>
2019-12-11 09:20:53 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var config = require('./config');
+var twit = require('twit');
+var T = new twit(config);
+
+var params = {
+	q: 'lol OR lmao OR 😂 since:2019-12-09',
+	count: 15,
+    lang: 'en'
+}
+
+T.get('search/tweets', params, function(err, data, response) {
+	var tweets = data.statuses;
+    if(!err){
+    	tweets.forEach(function(tweet){
+			console.log(tweet.in_reply_to_status_id_str);
+			console.log(tweet.favorite_count);
+			console.log(tweet.retweet_count);
+        	// if tweet is op, retweet or like
+        	if(!tweet.in_reply_to_status_id_str && tweet.favorite_count > 8){
+            	T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response){
+					if(response){
+						console.log('Retweeted a tweet!');
+					}
+                });
+            }
+            // if tweet is a reply, find parent tweet and retweet or like
+            if(tweet.in_reply_to_status_id_str && tweet.favorite_count > 1){
+                T.post('statuses/retweet/:id', { id: tweet.in_reply_to_status_id_str }, function (err, data, response){
+                	if(response){
+						console.log('Retweeted a tweet!');
+					}
+                });
+        	}
+    	});
+    }
+});

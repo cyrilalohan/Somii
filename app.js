93c02a7e64f5e4245f84531e956c000f1f8d2420
Osarumen Alohan <cyrilalohan@gmail.com>
2019-12-11 21:41:06 +0000

    add interval

--- app.js
+++ app.js
@@ -8,29 +8,30 @@ var params = {
     lang: 'en'
 }
 
-T.get('search/tweets', params, function(err, data, response) {
-	var tweets = data.statuses;
-    if(!err){
-    	tweets.forEach(function(tweet){
-			console.log(tweet.in_reply_to_status_id_str);
-			console.log(tweet.favorite_count);
-			console.log(tweet.retweet_count);
-        	// if tweet is op, retweet or like
-        	if(!tweet.in_reply_to_status_id_str && tweet.favorite_count > 8){
-            	T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response){
-					if(response){
-						console.log('Retweeted a tweet!');
-					}
-                });
-            }
-            // if tweet is a reply, find parent tweet and retweet or like
-            if(tweet.in_reply_to_status_id_str && tweet.favorite_count > 1){
-                T.post('statuses/retweet/:id', { id: tweet.in_reply_to_status_id_str }, function (err, data, response){
-                	if(response){
-						console.log('Retweeted a tweet!');
-					}
-                });
-        	}
-    	});
-    }
-});
+function findAndRetweet(){
+	T.get('search/tweets', params, function(err, data, response) {
+		var tweets = data.statuses;
+		if(!err){
+			tweets.forEach(function(tweet){
+				// if tweet is op, retweet or like
+				if(!tweet.in_reply_to_status_id_str && tweet.favorite_count > 8){
+					T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response){
+						if(response){
+							console.log('Retweeted a tweet!');
+						}
+					});
+				}
+				// if tweet is a reply, find parent tweet and retweet or like
+				if(tweet.in_reply_to_status_id_str && tweet.favorite_count > 1){
+					T.post('statuses/retweet/:id', { id: tweet.in_reply_to_status_id_str }, function (err, data, response){
+						if(response){
+							console.log('Retweeted a tweet!');
+						}
+					});
+				}
+			});
+		}
+	});
+}
+
+setInterval(findAndRetweet, 60000);
